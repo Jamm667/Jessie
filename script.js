7d5571aa6cac114d4147bae552642844505d2087
Jamm667 <130937547+Jamm667@users.noreply.github.com>
2023-12-07 01:55:00 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function displayAlert(){
+	alert('What were you expecting? There is no going back in life. Keep going. Well, its not like I can stop you from using the browsers back button or whatever')
+
+}
+
+function displayAlert2(){
+	alert('Uh hm, you did not came out just to go home did you?')
+
+}
+
+function displayAlert3(){
+	alert('Restart? I hope there is a restart whenever I messed up.Tip:try the browsers back button')
+
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var rellax = new Rellax('.rellax'); // Initialize Rellax with the class 'rellax'
+});
